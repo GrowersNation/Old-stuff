b55c631c02fdaf2683200514b417880ea1afbb86
Ben Keeping <benjaminkeeping@gmail.com>
2012-04-21 12:25:53 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrowersNation::Application.config.secret_token = 'c358ded5ef127bdeae25f6c459ba7dd39ab724c1a59266b7fe8495e6791686f7f77eef8c4b0d159fa95e396cc31a4bfc2c514f1ddec6db22f47a01d55b2c772a'
